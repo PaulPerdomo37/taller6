b90010df9facabd56ba73fb376cbd6f0237f0ebc
salva1984 <salvamusa2005@gmail.com>
2024-12-12 21:14:22 -0500

    Move Method Problema 3

--- src/Empresa.java
+++ src/Empresa.java
@@ -26,6 +26,17 @@ public class Empresa {
         return empleadosPorTipo;
     }
 
+
+    // MOVE METHOD
+    public static Empleado buscarEmpleadoPorNombre(String nombre, List<Empleado> empleados) {
+        for (Empleado empleado : empleados) {
+            if (empleado.getNombre().equals(nombre)) {
+                return empleado;
+            }
+        }
+        return null;
+    }
+
     // Más metodos
 }
 
