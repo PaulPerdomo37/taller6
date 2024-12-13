3143a3ce344b131d561d50bf79d3734e5794bd08
salva1984 <salvamusa2005@gmail.com>
2024-12-12 23:00:44 -0500

    a

--- src/taller/Main.java
+++ src/taller/Main.java
@@ -8,10 +8,16 @@ public class Main {
         Empresa empresa = new Empresa();
 
         Empleado emp1 = new EmpleadoPorHoras("Dario Laborde", 45, 15, "Sistemas", 23, "Masculino");
+
         // se usa el decorador para sobreescribir calcular salario con el bono
         emp1 = new DecoratorSistemas(emp1);
+
+
         Empleado emp2 = new EmpleadoFijo("Jorge Gaibor",700,45,"Contabilidad",40, "Masculino");
+        
+        // se usa el decorador para sobreescribir calcular salario con el bono
         emp2 = new DecoratorContabilidad(emp2);
+
         EmpleadoTemporario emp3 = new EmpleadoTemporario("Jordan Salinas",200,20,"Medico",6, "Masculino");
 
         empresa.contratarEmpleado(emp1);
